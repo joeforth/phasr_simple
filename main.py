9aa7452bbf368b8830b1671e00b1e7a638483b6e
Joe Forth <jwforth@gmail.com>
2025-10-07 16:54:05 +0100

    minimal example with images

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from pygrabber.dshow_graph import FilterGraph
 import processing
 import matplotlib.pyplot as plt
 
-# Initialize the camera
-cap = cv2.VideoCapture("noFeed.jpeg")
+# Initialise global variables
+img_path = None
+
 # Global variables to hold schedule intervals (in milliseconds) and current index
 intervals = []
 current_interval_index = 0
@@ -23,9 +24,26 @@ phase_name = ["Vials","foams","slightly turbids","transparent","very turbids"]
 description_entries = []
 timer_job = None
 
+def choose_input():
+    global img_path
+    # ======== File input =========
+    filetypes = [
+        ("Image Files", "*.jpg *.jpeg *.png *.tiff *.bmp"),
+    ]
 
+    img_path = filedialog.askopenfilename(title="Select media file", filetypes=filetypes)
+    if not img_path:
+        return
 
-def display_image(img):
+    img = cv2.imread(img_path)
+    if img is None:
+        return messagebox.showerror("Error", "Could not read image file.")
+    
+    update_feed()
+
+def update_feed():
+    img = cv2.imread(img_path)
+
     # Display a single image in the video panel
     # Convert from BGR (OpenCV) to RGB (Pillow/Tkinter)
     frame_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -35,38 +53,52 @@ def display_image(img):
     imgtk = ImageTk.PhotoImage(image=img_pil)
 
     # Update the Tkinter label (same widget as used for video)
-    video_panel.imgtk = imgtk       # keep a reference to avoid garbage collection
-    video_panel.configure(image=imgtk)
+    img_panel.imgtk = imgtk       # keep a reference to avoid garbage collection
+    img_panel.configure(image=imgtk)
 
-processed,new_count,finalVials = processing.ProcessImage(detected_frame,size_cm)
+def capture_and_detect():
+    global capture_count
+    global size_cm
+    global count
+    global folder
+
+    detected_frame = cv2.imread(img_path)
+
+    processed,new_count,finalVials = processing.ProcessImage(detected_frame,size_cm)
+    # Convert processed frame to RGB and update the detection panel
+    detected_rgb = cv2.cvtColor(processed, cv2.COLOR_BGR2RGB)
+    img_detected = Image.fromarray(detected_rgb)
+    imgtk_detected = ImageTk.PhotoImage(image=img_detected)
+    detected_panel.imgtk = imgtk_detected
+    detected_panel.configure(image=imgtk_detected)
+    capture_button.configure(state="normal")
 
 # Set up the main window
 root = tk.Tk()
-root.title("Camera Feed with Object Detection")
+root.title("Object Detection")
+root.geometry("1200x800")
 
-source_button = ttk.Button(root, text="Choose Input Source", command=choose_input)
+source_button = ttk.Button(root, text="Choose Input File", command=choose_input)
 source_button.pack(side="top", fill="x", padx=10, pady=10)
 
-# Create a panel for the live video feed
-video_panel = Label(root)
-video_panel.pack(side="left", padx=10, pady=10)
+# Create a panel for the image
+img_panel = Label(root)
+img_panel.pack(side="top", padx=10, pady=10)
 
 # Create a panel for the detection result
 detected_panel = Label(root)
-detected_panel.pack(side="right", padx=10, pady=10)
+detected_panel.pack(side="top", padx=10, pady=10)
 
 set_up = ttk.Button(root, text="Set Up Experiment", command=lambda: (size_cm.clear(),capture_and_detect()))
 set_up.pack(side="bottom", fill="both", padx=10, pady=10)
 
-# Button to manually capture an image and run object detection
-capture_button = ttk.Button(root, text="Detect", command=lambda: capture_and_detect(count),state="disabled")
+# Button to run object detection
+capture_button = ttk.Button(root, text="Detect", command=lambda: capture_and_detect(),state="disabled")
 capture_button.pack(side="bottom", fill="both", padx=10, pady=10)
 
 entry_container = tk.Frame(root)
 entry_container.pack(side="bottom", fill="x", padx=10, pady=10)
 
-update_feed()
 root.mainloop()
-cap.release()
 
 
