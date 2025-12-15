c5edfd1627d04393aa56e298740d9b283ad88606
Joe Forth <jwforth@gmail.com>
2025-12-15 11:36:46 +0000

    Update train.py

--- train.py
+++ train.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+# import os
+# os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
 
-model = YOLO("yolo11n.yaml")
+model = YOLO("yolo11l.yaml")
 
-
-
-results = model.train(task="detect",data="yourdataset/data.yaml",device = [0,1],
+results = model.train(task="detect",data="yourdataset/data.yaml", name="ShampooSTFA", device = [0,1], 
+                                                                              # Hyperparameters
                                                                               epochs=150,
                                                                               patience=150,
                                                                               batch=8,
@@ -15,25 +14,25 @@ results = model.train(task="detect",data="yourdataset/data.yaml",device = [0,1],
                                                                               bgr= 0.0,
                                                                               box= 0.1887,
                                                                               cls= 0.27033,
-                                                                              cutmix= 0.0,
-                                                                              degrees= 20.0,
-                                                                              fliplr= 0.5,
-                                                                              flipud= 0.0,
-                                                                              hsv_h= 0.0,
-                                                                              hsv_s= 0.6872304839691561,
-                                                                              hsv_v= 0.8889545777864033,
                                                                               lr0= 0.0010317,
                                                                               lrf= 0.64506,
-                                                                              mixup= 0.0,
                                                                               momentum= 0.64506,
-                                                                              mosaic= 0.0,
-                                                                              name= "ShampooSTFA",
-                                                                              optimizer= "AdamW",
-                                                                              perspective= 1.6132948989127868e-05,
-                                                                              scale= 0.5,
-                                                                              shear= 10.0,
-                                                                              translate= 0.1,
                                                                               warmup_epochs= 3.1,
+                                                                              optimizer= "AdamW",
                                                                               warmup_momentum= 0.45676,
-                                                                              weight_decay= 0.0004008345021896791
-                                                                                    ) 
+                                                                              weight_decay= 0.0005
+                                                                              # Augmentation hyperparameters
+                                                                              cutmix= 0.0,
+                                                                              degrees= 0.0,
+                                                                              fliplr= 0.5,
+                                                                              flipud= 0.0,
+                                                                              hsv_h= 0.3,
+                                                                              hsv_s= 0.6872304839691561,
+                                                                              hsv_v= 0.8889545777864033,
+                                                                              mixup= 0.0,
+                                                                              mosaic= 0.0,
+                                                                              perspective= 0.01,
+                                                                              scale= 0.5,
+                                                                              shear= 0.0,
+                                                                              translate= 0.5,
+                                                                              ) 
