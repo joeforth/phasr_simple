9889b30511ce4b346d2b4f970db27514dd18fdd1
Joe Forth <jwforth@gmail.com>
2026-07-06 11:00:24 +0100

    minor tweaks

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,6 @@ from ultralytics.data.split import autosplit
 
 autosplit(
     path="./data/images/",
-    weights=(0.7, 0.15, 0.15),  # (train, validation, test) fractional splits
-    annotated_only=False,  # split only images with annotation file when True
+    weights=(0.8, 0.1, 0.1),  # (train, validation, test) fractional splits
+    annotated_only=True,  # split only images with annotation file when True
 )
